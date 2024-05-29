7dfee6924b0ca251f4f7de5dfc43d63878cb5d44
Fernando Salazar <fs182@hotmail.com>
2024-05-29 17:22:39 -0500

    Ajustes

--- CodeGenerator.Infrastructure/obj/Debug/net8.0/CodeGenerator.Infrastructure.AssemblyInfo.cs
+++ CodeGenerator.Infrastructure/obj/Debug/net8.0/CodeGenerator.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeGenerator.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7438c2fe90328eb9202e7109a256a2eae2183c2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeGenerator.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeGenerator.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
